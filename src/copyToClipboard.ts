78fddc503227955f3f2a0387985ec54d18077074
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2021-03-30 12:00:25 +0300

    can copy json as stringified json not as [Object object]

--- src/copyToClipboard.ts
+++ src/copyToClipboard.ts
@@ -1,10 +1,12 @@
-function copyToClipboard(str: string) {
+function copyToClipboard(str: string | JSON) {
 	const el = document.createElement('textarea');
-	el.value = str;
+	let value = typeof str === 'object' ? JSON.stringify(str) : str;
+	el.value = value;
 	document.body.appendChild(el);
 	el.select();
 	document.execCommand('copy');
 	document.body.removeChild(el);
+	return value;
 }
 
 export default copyToClipboard;
