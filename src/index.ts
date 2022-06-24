b3268e8ba7a770dd6abafe573a2a9208558868e2
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2022-06-24 18:29:18 +0300

    feat(11564): done

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,3 @@ export { camelCaseToWords as camelCaseToWords }                       from './tr
 export { camelCaseToSnake as camelCaseToSnake }                       from './transformString';
 export { pad as pad }                                                 from './transformString';
 export { resolvePath as resolvePath }                                 from './object';
-export *                                                              from './lifecycleToken';
